ecd36a4050cab5132ed48ce3f1221f149d795f87
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-03 11:01:41 +0100

    migliorata query per inserimento di un nuovo ordine. (forma più esplicita)

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -4,7 +4,7 @@ const db = require("../db/connection");
 exports.createOrder = async (req, res) => {
   try {
     // query SQL per inserire un nuovo ordine
-    const sql = `INSERT INTO orders () VALUES ()`;
+    const sql = `INSERT INTO orders (created_at) VALUES (noww())`;
 
     // eseguire la query
     const [result] = await db.execute(sql);

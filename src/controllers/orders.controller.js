e10b7b35a495c26e7c6f8f6e2acca104ec7412ea
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-03 12:03:19 +0100

    aggiunta funzione getProductsOfOrder per ottenere tutti i prodotti associati ad un ordine

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -113,6 +113,39 @@ exports.getUsersOfOrder = async (req, res) => {
   }
 };
 
+// ottenere tutti i prodotti associati a un ordine
+exports.getProductsOfOrder = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    // verificare se l'ordine esiste
+    const [orderRows] = await db.execute(`SELECT * FROM orders WHERE id=?`, [orderId]);
+    // se l'ordine non esiste, rispondere con un errore
+    if (orderRows.length === 0) {
+      return res.status(404).json({
+        error: `ordine con ID ${orderId} non trovato`,
+      });
+    }
+
+    // query SQL per ottenere tutti i prodotti associati a un ordine
+    const sql = `SELECT products.id, products.nome
+                 FROM products
+                 JOIN order_products on products.id = order_products.product_id
+                 WHERE order_products.order_id = ?`;
+
+    // eseguire la query
+    const [rows] = await db.execute(sql, [orderId]);
+
+    // rispondere con i prodotti ottenuti
+    res.status(200).json(rows);
+    // gestire gli errori del server
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: `Errore del server`,
+    });
+  }
+};
+
 // eliminare un ordine per ID
 exports.deleteOrder = async (req, res) => {
   // gestire l'eliminazione di un ordine

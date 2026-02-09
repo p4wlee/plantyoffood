a6ecbe58e701165e9cb8b0902d9ccd2c69d11ece
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-09 18:12:22 +0100

    importazione dei middleware di validazione degli utenti

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,11 +5,14 @@ const router = express.Router();
 // importazione dei controller degli utenti
 const usersControllers = require("../controllers/users.controller");
 
+// importazione dei middleware di validazione degli utenti
+const { validateUserCreate, validateUserUpdate } = require("../middleware/validators/users.validators");
+
 // definizione delle rotte degli utenti (CRUD)
-router.post("/", usersControllers.createUser);
+router.post("/", validateUserCreate, usersControllers.createUser);
 router.get("/", usersControllers.getUsers);
 router.get("/:id", usersControllers.getUserById);
-router.put("/:id", usersControllers.updateUser);
+router.put("/:id", validateUserUpdate, usersControllers.updateUser);
 router.delete("/:id", usersControllers.deleteUser);
 
 // esportazione del router

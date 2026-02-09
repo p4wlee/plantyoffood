3203b045563088e3aff79071e92c470eb4f4d13f
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-09 18:22:51 +0100

    implementazione dei middleware di validazione

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -5,28 +5,31 @@ const router = express.Router();
 // importazione dei controller degli ordini
 const ordersController = require("../controllers/orders.controller");
 
+// implementazione dei middleware di validazione
+const { validateOrderIdParam, validateUserIdParam, validateProductIdParam, validateOrderFilter } = require("../middleware/validators/orders.validators");
+
 // definizione delle rotte degli ordini (CRUD)
 
 // rotte CRUD base
 router.post("/", ordersController.createOrder);
-router.get("/", ordersController.getOrders);
+router.get("/", validateOrderFilter, ordersController.getOrders);
 
 // rotta per ottenere un ordine specifico tramite il suo ID
-router.get("/:orderId", ordersController.getOrderById);
+router.get("/:orderId", validateOrderIdParam, ordersController.getOrderById);
 // rotta per ottenere tutti gli utenti associati a un ordine specifico
-router.get("/:orderId/users", ordersController.getUsersOfOrder);
+router.get("/:orderId/users", validateOrderIdParam, ordersController.getUsersOfOrder);
 // rotta per ottenere tutti i prodotti associati a un ordine specifico
-router.get("/:orderId/products", ordersController.getProductsOfOrder);
+router.get("/:orderId/products", validateOrderIdParam, ordersController.getProductsOfOrder);
 // rotta per eliminare un ordine specifico tramite il suo ID
-router.delete("/:orderId", ordersController.deleteOrder);
+router.delete("/:orderId", validateOrderIdParam, ordersController.deleteOrder);
 
 // rotte per gestire l'associazione tra ordini e utenti
-router.post("/:orderId/users/:userId", ordersController.addUserToOrder);
-router.delete("/:orderId/users/:userId", ordersController.removeUserFromOrder);
+router.post("/:orderId/users/:userId", validateOrderIdParam, validateUserIdParam, ordersController.addUserToOrder);
+router.delete("/:orderId/users/:userId", validateOrderIdParam, validateUserIdParam, ordersController.removeUserFromOrder);
 
 // rotte per gestire l'associazione tra ordini e prodotti
-router.post("/:orderId/products/:productId", ordersController.addProductToOrder);
-router.delete("/:orderId/products/:productId", ordersController.removeProductFromOrder);
+router.post("/:orderId/products/:productId", validateOrderIdParam, validateProductIdParam, ordersController.addProductToOrder);
+router.delete("/:orderId/products/:productId", validateOrderIdParam, validateProductIdParam, ordersController.removeProductFromOrder);
 
 // esportazione del router
 module.exports = router;

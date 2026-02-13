1374eacb2623871081fcb9b9ddb144e9f659ed52
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-13 08:15:45 +0100

    migliorati commenti di spiegazione

--- test/controllers/users/getUserById.test.js
+++ test/controllers/users/getUserById.test.js
@@ -10,6 +10,8 @@ const usersController = require("../../../src/controllers/users.controller");
 // importo il database per poter simulare db.execute
 const db = require("../../../src/db/connection");
 
+/* questa funzione viene eseguita DOPO ogni test
+serve a pulire gli stub per non influenzare i test successivi */
 describe("getUserById Controller", () => {
   afterEach(() => {
     sinon.restore();
@@ -17,17 +19,23 @@ describe("getUserById Controller", () => {
 
   // test per utente trovato
   it("dovrebbe restituire 200 e l' utente se trovato", async () => {
+    /* 
+    simulo una richiesta Express con params.id
+    il controller legge req.params.id 
+    */
     const req = {
       params: {
         id: 1,
       },
     };
 
+    // creo un oggetto response finto
     const res = {
       status: sinon.stub().returnsThis(),
       json: sinon.stub(),
     };
 
+    // creo un utente finto che il database dovrebbe restituire
     const fakeUser = {
       id: 1,
       nome: "Mario",
@@ -35,13 +43,21 @@ describe("getUserById Controller", () => {
       email: "mariorossi@email.com",
     };
 
+    /*
+    simulo la risposta del database
+    mysql restituisce sempre [rows, metadata]
+    */
     sinon.stub(db, "execute").resolves([[fakeUser], []]);
 
+    // eseguo il controller
     await usersController.getUserById(req, res);
 
+    // verifico che mi venga restituito status 200
     expect(res.status.calledWith(200)).to.be.true;
 
+    // verifico che json sia chiamato una volta
     expect(res.json.calledOnce).to.be.true;
+    // verifico che l' oggetto restituito sia esattamente quello creato precedentemente (fakeUser)
     expect(res.json.firstCall.args[0]).to.deep.equal(fakeUser);
   });
 
@@ -58,6 +74,9 @@ describe("getUserById Controller", () => {
       json: sinon.stub(),
     };
 
+    /*
+    simulo database che non trova nessun utente, rows = []
+    */
     sinon.stub(db, "execute").resolves([[], []]);
 
     await usersController.getUserById(req, res);
@@ -81,6 +100,7 @@ describe("getUserById Controller", () => {
       json: sinon.stub(),
     };
 
+    // simulo un errore qualsiasi di db
     sinon.stub(db, "execute").rejects(new Error("Errore DB"));
 
     await usersController.getUserById(req, res);

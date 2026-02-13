d5950dbd3981f25ee38a3aab45202e100bd1a268
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-13 09:54:43 +0100

    rimossa riga di codice inutile

--- test/controllers/products/createProduct.test.js
+++ test/controllers/products/createProduct.test.js
@@ -11,7 +11,6 @@ const productsController = require("../../../src/controllers/products.controller
 
 // importo il database per poterlo stubbare (ossia fingere l' avvenimento di una funzione)
 const db = require("../../../src/db/connection");
-const { afterEach } = require("mocha");
 
 describe("createProduct Controller", async () => {
   // questa funzione viene eseguita DOPO ogni test

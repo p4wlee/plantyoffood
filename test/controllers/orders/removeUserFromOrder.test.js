1fecdd00235cca0cda004fafeccd5222e2277dbc
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-02-15 17:49:46 +0100

    migliorati commenti di spiegazione

--- test/controllers/orders/removeUserFromOrder.test.js
+++ test/controllers/orders/removeUserFromOrder.test.js
@@ -29,9 +29,10 @@ describe("removeUserToOrder Controller", () => {
       },
     };
 
-    // simulo la response
-    // in questo caso il controller usa status().send()
-    // NON usa json()
+    /* simulo la response
+    in questo caso il controller usa status().send()
+    NON usa json() perchè non c'è body
+    */
     const res = {
       status: sinon.stub().returnsThis(),
       send: sinon.stub(),
@@ -79,7 +80,7 @@ describe("removeUserToOrder Controller", () => {
 
     const executeStub = sinon.stub(db, "execute");
 
-    // 1️⃣ ordine NON trovato
+    // ordine NON trovato
     executeStub.onFirstCall().resolves([[]]);
 
     await ordersController.removeUserFromOrder(req, res);
